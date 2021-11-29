9d521436942450ffea0fb4df470e43d8ee57005d
Rob van Bakel <robbakel@gmail.com>
2021-11-29 15:13:01 +0100

    Add tips on 404-page

--- 404.js
+++ 404.js
@@ -1,4 +1,8 @@
 const codeSpan = document.querySelector('h1 span')
+const desc = document.querySelector('.description')
+const tip = document.querySelector('.tip')
+
+const allowedCharacters = /^[a-z0-9_-]+$/;
 
 const code = window.location.search.substring(1,)
 
@@ -9,3 +13,11 @@ if(!code) {
 }
 
 codeSpan.textContent = code
+
+if (code.length < 4 || code.length > 32) {
+  desc.innerHTML = 'Check your link and try again!'
+  tip.innerHTML = '<strong>TIP:</strong><br>GoTiny links can only be between 4-32 characters long.'
+} else if (!allowedCharacters.test(code)) {
+  desc.innerHTML = 'Check your link and try again!'
+  tip.innerHTML = '<strong>TIP:</strong><br>GoTiny links can only consist out of lowercase letters, numbers and the <span class="char">-</span> and <span class="char">_</span> symbols.'
+}
