b7984e058f317e5e31a6bb0bc0a2b9e48d4b3569
Rob van Bakel <robbakel@gmail.com>
2024-04-05 22:27:13 +0200

    Add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,11 +9,26 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: "GoTiny | The most lightweight link shortener API",
       link: [
         {
           rel: "icon",
           type: "image/png",
           href: "/favicon.png",
+        },
+      ],
+      meta: [
+        {
+          property: "og:title",
+          content: "GoTiny | The most lightweight link shortener API",
+        },
+        {
+          property: "og:url",
+          content: "https://www.gotiny.cc",
+        },
+        {
+          property: "og:title",
+          content: "/gotiny_banner.jpg",
         },
       ],
     },
