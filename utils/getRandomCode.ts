e595d9214cf200b4405b9f26f5f9f3deccb7ad39
Rob van Bakel <robbakel@gmail.com>
2024-05-03 17:43:48 +0200

    Use nanoid

--- utils/getRandomCode.ts
+++ utils/getRandomCode.ts
@@ -1,10 +1,9 @@
-const chars = "abcdefhkmnprstwxyz2345678";
-const charsArray = chars.split("");
+import { customAlphabet } from "nanoid";
+
+const nanoid = customAlphabet("abcdefhkmnprstwxyz2345678");
 
 export default (length = 6): string => {
-  const code = Array.from({ length })
-    .map(() => charsArray[Math.floor(Math.random() * chars.length)])
-    .join("");
+  const code = nanoid(length);
 
   return code;
 };
